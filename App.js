33591fd1e61dc47e0ccaaf7819d2b4ff716263f1
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-11-15 01:57:48 +0500

    card

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from 'react-native'
 import React from 'react'
 
 export default function App() {
@@ -13,8 +13,23 @@ export default function App() {
                     </View>
                 </TouchableOpacity>
             </View>
-            <View style={styles.two}>
 
+            <View style={styles.two}>
+                <ScrollView>
+                    <View style={styles.card}>
+                        <View>
+                            <Text style={styles.cardText} >ss</Text>
+                            <Text style={styles.cardText1} >ss</Text>
+                        </View>
+                        <View>
+                            <View></View>
+                            <View></View>
+                        </View>
+
+                    </View>
+
+
+                </ScrollView>
             </View>
 
 
@@ -36,26 +51,45 @@ const styles = StyleSheet.create({
         flex: 0,
         backgroundColor: '#9395D3',
         flexDirection: 'row',
-    justifyContent: 'space-between',
+        justifyContent: 'space-between',
     }
     , two: {
+        paddingTop: 10,
         flex: 6,
         backgroundColor: "#D6D7EF",
     }, main: {
         flex: 1,
-    },button:{
-        alignItems:"center",
-        justifyContent:"center",
+    }, button: {
+        alignItems: "center",
+        justifyContent: "center",
         color: '#FFF',
         backgroundColor: '#D6D7EF',
-        borderRadius:50,
-        margin:5,
+        borderRadius: 50,
+        margin: 5,
         width: 45,
         height: 45,
-    },buttonText:{
+    }, buttonText: {
         color: '#FFF',
         fontSize: 24,
         // padding:10
-    }
+    }, card: {
+        marginHorizontal: 10,
+        marginVertical: 10,
+        backgroundColor: '#FFFFFF',
+        borderRadius: 15,
+        padding: 10,
+    }, cardText: {
+        color: '#9395D3',
+        fontFamily: 'Jost',
+        fontSize: 20,
+        fontStyle: 'normal',
+        fontWeight: '600',
+    }, cardText1: {
+        color: '#000',
+        fontFamily: 'Jost',
+        fontSize: 10,
+        fontStyle: 'normal',
+        fontWeight: '400',
+    },
 
 })
